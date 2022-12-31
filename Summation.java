97959c032c259580dce17ab094942192f6cffd58
Sukanya <106397577+Gund008@users.noreply.github.com>
2022-12-31 20:24:29 +0530

    Add files via upload

--- Summation.java
+++ Summation.java
@@ -1,82 +1,69 @@
-//Accept N number from user and Return Summation of all Array Elements.
-
-import java.lang.*;
-import java.util.Scanner;
-
 /*
- * Below Concept used in Class Designing :-
- *       Encapsulation:
- *       Inheritance:(Single Level)
- *       keyword:this,super 
- *       Access Specifier:Protected
+ * Write a Java Program which Accept two Arrays from  user and display Summation of each Array.
+ * Input:2 9 7 5 2 3
+ *       9 3 5 5 
+ * 
+ * Output:28  22
  */
-class ArrayX
-{
-    protected int Arr[];
 
-    public ArrayX(int iSize)
-    {
-        this.Arr=new int [iSize];
-    }
+ import java.util.*;
 
-    protected void  accept()
-    {
-        Scanner sobj=new Scanner(System.in);
-        System.out.println("Please Enter"+Arr.length +"Elements");
-    
-        for(int iCnt=0;iCnt<Arr.length;iCnt++)
-        {
-               System.out.println("Enter the Elements Number:"+(iCnt + 1));  
-               Arr[iCnt]=sobj.nextInt();
-        }
-    }
-
-    protected void display()
-    {
-        System.out.println("Array Elements are:");
-        for(int iCnt=0;iCnt<Arr.length;iCnt++)
-        {
-            System.out.print("\t"+Arr[iCnt]);
-        }
-        System.out.println();   
-    } 
-}
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-class MarvellousX extends ArrayX
-{
-    public MarvellousX(int i)
-    {
-        super(i);
-    }
-
-    public int sumArrayElements() 
-    {
-        int iCnt=0;
-        int iSum=0;
-        for(iCnt=0;iCnt<Arr.length;iCnt++)
-        {
-            iSum=iSum + Arr[iCnt];
-        }
-        return iSum;
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-
-class  Summation
-{
-    public static void main(String arg[])
-    {
-        Scanner sobj=new Scanner(System.in);
-        System.out.println("Please Enter size of Array:");
-        int iNo=sobj.nextInt();
-
-        MarvellousX aobj=new MarvellousX(iNo);
-        
-        aobj.accept();
-        aobj.display();
-        int iRet=aobj.sumArrayElements();
-        System.out.println("Summation of Array Elements:"+iRet);
-    }
-}
+ class MyArray
+ {
+     public int Arr1[];
+     public int Arr2[];
+ 
+     public MyArray(int iValue1,int iValue2)
+     {
+         Arr1=new int[iValue1];
+         Arr2=new int[iValue2];
+     }
+     public void accept()
+     {
+         Scanner sobj=new Scanner(System.in);
+         int iCnt=0;
+         for(iCnt=0;iCnt<Arr1.length;iCnt++)
+         {
+             System.out.print("Array Elements:"+(iCnt+1));
+             Arr1[iCnt]=sobj.nextInt();
+         }
+         System.out.println("");
+         for(int iCnt1=0;iCnt1<Arr2.length;iCnt1++)
+         {
+             System.out.print("Array Elements:"+(iCnt1+1));
+             Arr2[iCnt1]=sobj.nextInt();
+         }
+         System.out.println("");
+     }
+ 
+     public void displaySum()
+    { 
+         int iCnt=0;
+         int iSum=0;
+         int iSum1=0;
+         for(iCnt=0;iCnt<Arr1.length;iCnt++)
+         {
+              iSum=iSum + Arr1[iCnt];
+         }
+         System.out.print(iSum+"\t");
+         for(int iCnt1=0;iCnt1<Arr2.length;iCnt1++)
+         {
+              iSum1=iSum1 + Arr2[iCnt1];
+         }
+         System.out.print(iSum1+"\t");
+         System.out.println("");
+     }
+ }
+ ///////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+ class Program5
+ {
+     public static void main(String arg[])
+     {
+         int iNo1=6;
+         int iNo2=4;
+         MyArray mobj=new MyArray(iNo1,iNo2);
+         mobj.accept();
+         mobj.displaySum();
+     }
+ }
